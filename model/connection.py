3fa01ace9c7b67812ce2ae240aa868352eba4fad
brunoharlein <bruno@harlein.com>
2020-02-05 14:12:57 +0100

    update connection class with make_request method

--- model/connection.py
+++ model/connection.py
@@ -35,3 +35,41 @@ class Connection():
         if(self.connection):
             self.cursor.close()
             self.connection.close()
+
+    def make_request(self, sql, arguments=False, message=False):
+        """General method to handle a sql request with arguments and an error message
+            Méthode générale pour gérer une requête SQL avec des arguments et un message d'erreur"""
+        try:
+            self.initialize_connection()
+            # execute the given SQL query with arguments if there are
+            # exécuter la requête SQL donnée avec des arguments s'il y a
+            self.cursor.execute(sql, arguments)
+            # if the request is of type SELECT we return a fetch with all results
+            # si la demande est de type SELECT, nous renvoyons une recherche avec tous les résultats
+            if sql.lower().startswith("select", 0):
+                return self.cursor.fetchall()
+            # if it is not a select then we have to commit whatever happens
+            # si ce n'est pas un SELECT, nous devons engager(enregistrer) quoi qu'il arrive
+            self.connection.commit()
+            # if the request is of type DELETE we have to know if something has been deleted
+            # si la requête est de type DELETE nous devons savoir si quelque chose a été supprimé
+            if sql.lower().startswith("delete", 0):
+                # if nothing has been deleted
+                # si rien n'a été effacer
+                if self.cursor.rowcount == 0:
+                    raise Exception("Nothing found")
+                return self.cursor.rowcount
+            # if the request was of type INSERT INTO and was a success then return True
+            # si la requête était de type INSERT INTO et a été un succès, retournez True
+            return True
+        except Exception as e:
+            # If a specefic message has been given print it otherwise print the exception
+            # Si un message spécifique a été donné, imprimez-le sinon imprimez l'exception
+            if message:
+                print(message)
+            else:
+                print(e)
+            return False
+        finally:
+            self.close_connection()
+
